61e211d3afd1a066911aacff03ab3bca65ceb3d2
aridane <aridanecabreraramirez@gmail.com>
2023-09-22 19:10:51 +0200

    Activiad A4 Finalizada

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/home', function () {
     return view('home');
  })->middleware(['auth', 'verified'])->name('home');
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

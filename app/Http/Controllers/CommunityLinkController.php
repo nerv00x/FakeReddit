35267628aef53a1a8fb5daf7dd3c025b84bd38f5
aridane <aridanecabreraramirez@gmail.com>
2023-09-22 19:39:58 +0200

    A5_1

---
+++ app/Http/Controllers/CommunityLinkController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CommunityLink;
+use Illuminate\Http\Request;
+
+class CommunityLinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CommunityLink $communityLink)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CommunityLink $communityLink)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CommunityLink $communityLink)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CommunityLink $communityLink)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,6 @@ Route::get('/home', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('community', [App\Http\Controllers\CommunityLinkController::class, 'index']);
+Route::post('community', [App\Http\Controllers\CommunityLinkController::class, 'store']);
